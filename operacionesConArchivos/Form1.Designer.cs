6f795ad8dad88037037c83415fd9cbaee4cded02
Josue Torres <josuep3b@hotmail.com>
2019-08-21 03:30:01 -0500

    Comenzando el proyecto

---
+++ operacionesConArchivos/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace operacionesConArchivos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cambiarNombreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eliminarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.labelArchEditandose = new System.Windows.Forms.Label();
+            this.textBoxTest = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevoToolStripMenuItem,
+            this.abrirToolStripMenuItem,
+            this.guardarToolStripMenuItem,
+            this.cambiarNombreToolStripMenuItem,
+            this.eliminarToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            this.archivoToolStripMenuItem.Click += new System.EventHandler(this.archivoToolStripMenuItem_Click);
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            this.nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            this.nuevoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.nuevoToolStripMenuItem.Text = "Nuevo";
+            this.nuevoToolStripMenuItem.Click += new System.EventHandler(this.nuevoToolStripMenuItem_Click);
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            this.guardarToolStripMenuItem.Click += new System.EventHandler(this.guardarToolStripMenuItem_Click);
+            // 
+            // cambiarNombreToolStripMenuItem
+            // 
+            this.cambiarNombreToolStripMenuItem.Name = "cambiarNombreToolStripMenuItem";
+            this.cambiarNombreToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cambiarNombreToolStripMenuItem.Text = "Cambiar Nombre";
+            this.cambiarNombreToolStripMenuItem.Click += new System.EventHandler(this.cambiarNombreToolStripMenuItem_Click);
+            // 
+            // eliminarToolStripMenuItem
+            // 
+            this.eliminarToolStripMenuItem.Name = "eliminarToolStripMenuItem";
+            this.eliminarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.eliminarToolStripMenuItem.Text = "Eliminar";
+            this.eliminarToolStripMenuItem.Click += new System.EventHandler(this.eliminarToolStripMenuItem_Click);
+            // 
+            // labelArchEditandose
+            // 
+            this.labelArchEditandose.AutoSize = true;
+            this.labelArchEditandose.Location = new System.Drawing.Point(12, 428);
+            this.labelArchEditandose.Name = "labelArchEditandose";
+            this.labelArchEditandose.Size = new System.Drawing.Size(116, 13);
+            this.labelArchEditandose.TabIndex = 1;
+            this.labelArchEditandose.Text = "Ningun Archivo Abierto";
+            this.labelArchEditandose.Click += new System.EventHandler(this.labelArchEditandose_Click);
+            // 
+            // textBoxTest
+            // 
+            this.textBoxTest.Location = new System.Drawing.Point(248, 167);
+            this.textBoxTest.Multiline = true;
+            this.textBoxTest.Name = "textBoxTest";
+            this.textBoxTest.Size = new System.Drawing.Size(312, 90);
+            this.textBoxTest.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(368, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Escribe algo aqui";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxTest);
+            this.Controls.Add(this.labelArchEditandose);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Operaciones Basicas";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cambiarNombreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eliminarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.Label labelArchEditandose;
+        private System.Windows.Forms.TextBox textBoxTest;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ operacionesConArchivos/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace operacionesConArchivos
+{
+    public partial class Form1 : Form
+    {
+        public string nombreArchivo;
+        FileStream archivo;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void labelArchEditandose_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void archivoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void nuevoToolStripMenuItem_Click(object sender, EventArgs e)//crea nuevo archivo 
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            if(saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                nombreArchivo = saveDialog.FileName;
+                archivo = new FileStream(nombreArchivo + ".BIN", FileMode.CreateNew,FileAccess.Write);
+                BinaryWriter bw = new BinaryWriter(archivo);
+                long cab = -1;
+                bw.Write("-1");
+                archivo.Close();
+
+
+              /*  archivo = new FileStream(nombreArchivo + ".BIN", FileMode.Open, FileAccess.Read); //abrimos el archivo para que este listo para editarlo                 
+                labelArchEditandose.Text = "Abierto Archivo: " + nombreArchivo; //actualizmaos el label para saber que archivo esta abierto!
+                archivo.Close();*/
+
+
+            }             
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)//Boton Abrir 
+        {
+            OpenFileDialog d = new OpenFileDialog();
+
+            d.Filter = "Archivos de binarios|*.BIN";
+            if (d.ShowDialog() != DialogResult.OK) return;
+
+            nombreArchivo = d.FileName; //recuperamos el nombre desde la ventana de dialogo
+
+            archivo = new FileStream(nombreArchivo, FileMode.Open, FileAccess.Read); //abrimos el archivo 
+            
+
+            labelArchEditandose.Text = "Abierto Archivo: "+ d.FileName; //actualizmaos el label para saber que archivo esta abierto!
+
+            archivo.Seek(0, SeekOrigin.Begin);
+            BinaryReader br = new BinaryReader(archivo);
+            long tamañoArch = archivo.Length;
+            char[] leidos = br.ReadChars((int)tamañoArch);
+            string sLeidos = new string(leidos);
+            textBoxTest.Text = sLeidos;
+            archivo.Close();
+        }
+
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e) //Guardar
+        {
+            if(labelArchEditandose.Text == "Ningun Archivo Abierto")
+            {        
+                MessageBox.Show("No se ha abierto ningun archivo", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                archivo = new FileStream(nombreArchivo, FileMode.Open, FileAccess.Write); //abrimos el archivo 
+                BinaryWriter bw = new BinaryWriter(archivo);
+                archivo.Seek(0, SeekOrigin.Begin);
+                bw.Write(textBoxTest.Text);
+                archivo.Close();
+            }
+        }
+
+        private void cambiarNombreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (labelArchEditandose.Text == "Ningun Archivo Abierto")
+            {
+                MessageBox.Show("No se ha abierto ningun archivo", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                SaveFileDialog saveDialog = new SaveFileDialog();
+                if (saveDialog.ShowDialog() == DialogResult.OK)
+                {
+                    FileStream nuevo = new FileStream(saveDialog.FileName + ".BIN", FileMode.CreateNew, FileAccess.ReadWrite);
+                    archivo = new FileStream(nombreArchivo, FileMode.Open, FileAccess.ReadWrite);
+
+                    archivo.CopyTo(nuevo);
+                    archivo.Close();
+                    nuevo.Close();
+                    File.Delete(nombreArchivo);
+
+                    nombreArchivo = saveDialog.FileName; //Actualizamos el nuevo nombre del archivo
+                    labelArchEditandose.Text = "Abierto Archivo: " + nombreArchivo; //y el label
+                }
+            }
+        }
+
+        private void eliminarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (labelArchEditandose.Text == "Ningun Archivo Abierto")
+            {
+                MessageBox.Show("No se ha abierto ningun archivo", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (MessageBox.Show("Esta seguro que desea eliminar el archivo", "Eliminar", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    File.Delete(nombreArchivo);
+                    labelArchEditandose.Text = "Ningun Archivo Abierto";
+                    textBoxTest.Clear();
+                }
+            }             
+        }
+    }
+}
